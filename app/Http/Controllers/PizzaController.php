be0cc71c9eba839dfb1055a98028357fd1ffb5a0
Danilo Andrés Granda Farfán <danilogrnd@gmail.com>
2022-02-20 11:58:24 -0500

    pizza added feature

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PizzaStoreRequest;
-use Validator;
+use App\Models\Pizza;
 use Illuminate\Http\Request;
 
 
@@ -16,7 +16,7 @@ class PizzaController extends Controller
      */
     public function index()
     {
-        return "List of pizza";
+        return view('pizza.index');
     }
 
     /**
@@ -37,7 +37,19 @@ class PizzaController extends Controller
      */
     public function store(PizzaStoreRequest $request)
     {
-        
+        $path = $request->image->store('public/pizza');
+        //dd($request->all());
+        Pizza::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'small_pizza_price' => $request->small_pizza_price,
+            'medium_pizza_price' => $request->medium_pizza_price,
+            'large_pizza_price' => $request->large_pizza_price,
+            'category' => $request->category,
+            'image' => $path,
+        ]);
+
+        return redirect()->route('pizza.index')->with('message', 'Pizza added successfully!');
     }
 
     /**
--- resources/views/pizza/create.blade.php
+++ resources/views/pizza/create.blade.php
@@ -16,21 +16,24 @@
                     </div>
                 </div>
             </div>
+            @if(count($errors)>0)
+            <div class="card mt-5">
+                <div class="card-body">
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <p>{{$error}}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            @endif
         </div>
 
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Pizza</div>
 
-                @if(count($errors)>0)
-                    <div class="alert alert-danger">
-                        @foreach($errors->all() as $error)
-                            <p>{{$error}}</p>
-                        @endforeach
-                    </div>
-                @endif
-
-                <form action="{{route('pizza.store')}}" method="post">@csrf
+                <form action="{{route('pizza.store')}}" method="post" enctype="multipart/form-data">@csrf
 
                 <div class="card-body">
                     <div class="form-group mt-2">
