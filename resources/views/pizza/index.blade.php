61932091917eb396b24af168628ad60c04a28930
Danilo Andrés Granda Farfán <danilogrnd@gmail.com>
2022-02-22 07:43:44 -0500

    admin orders dashboard

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -3,6 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
+    <div class="col-md-2">
+            <div class="card">
+                <div class="card-header">Menu</div>
+
+                <div class="card-body">
+                    <div class="form-group">
+                        <ul class="list-group">
+                            <a href="{{route('pizza.index')}}" class="list-group-item list-group-item-action">View</a>
+                            <a href="{{route('pizza.create')}}" class="list-group-item list-group-item-action">Create</a>
+                            <a href="{{route('user.order')}}" class="list-group-item list-group-item-action">User Order</a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            @if(count($errors)>0)
+            <div class="card mt-5">
+                <div class="card-body">
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <p>{{$error}}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            @endif
+        </div>
+
         <div class="col-md-10">
             <div class="card">
                 <div class="card-header">All Pizza
